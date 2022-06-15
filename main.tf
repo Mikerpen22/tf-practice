bb737ad516b7b70c9bdcbdb3d5c3b80845a7ad13
Minhung Shih <minhungs@zillowgroup.com>
2022-06-15 00:19:51 -0700

    finish main.tf & fix error connecting to server by replacing apt with yum

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+
+
+
 provider "aws" {
     // access and secret token are stored through aws cli config
     // https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html
@@ -16,24 +19,25 @@ resource "aws_vpc" "test-vpc" {
 # 2. Create Internet Gateway (so VPC can talk to outside world)
 resource "aws_internet_gateway" "test-gw" {
   vpc_id = aws_vpc.test-vpc.id
-
   tags = {
     Name = "test"
   }
 }
 
-# 3. Create custom route table
+# 3. Create custom route table for this VPC
 resource "aws_route_table" "test-route-table" {
   vpc_id = aws_vpc.test-vpc.id
 
+  // routing rules for ipv4
+  // default to route all traffic to gateway
   route {
-    cidr_block = "0.0.0.0/0"    // Default route all traffic to gateway
+    cidr_block = "0.0.0.0/0"    
     gateway_id = aws_internet_gateway.test-gw.id
   }
 
   route {
     ipv6_cidr_block        = "::/0"
-    egress_only_gateway_id = aws_internet_gateway.test-gw.id
+    gateway_id = aws_internet_gateway.test-gw.id
   }
 
   tags = {
@@ -41,26 +45,26 @@ resource "aws_route_table" "test-route-table" {
   }
 }
 
-# 4. Create a subnet
+# 4. Create a subnet within VPC
 resource "aws_subnet" "test-subnet-1" {
   vpc_id     = aws_vpc.test-vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-west-2a"
   tags = {
     Name = "test"
-
   }
 }
 
 # 5. Associate subnet with route table
+// This subnet uses this routing table
 resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.test-subnet-1.id
   route_table_id = aws_route_table.test-route-table.id
 }
 
 
-# 6. Create security group to allow port 22(for ssh), 80(Http), 443(Https)
-// Security Groups ensure that all the traffic flowing at the instance level is only through your established ports and protocols.
+# 6. Create security group to allow port 22(for ssh), 80(Http), 443(Https) traffic
+// Security Groups ensure all the traffic flowing at the instance level only through established ports and protocols.
 resource "aws_security_group" "allow_web_traffic" {
   name        = "allow_web_traffic"
   description = "allow web traffic"
@@ -101,6 +105,8 @@ resource "aws_security_group" "allow_web_traffic" {
 }
 
 # 7. Create a network interface with an ip in the subnet that was created in step 4
+// Network Interface is usually attached to an instance
+// An elastic network interface is a component in VPC that represents a virtual network card
 resource "aws_network_interface" "test-interface" {
   subnet_id       = aws_subnet.test-subnet-1.id
   private_ips     = ["10.0.1.50"] // for host
@@ -118,17 +124,27 @@ resource "aws_eip" "lb" {
 }
 
 # 9. Create Ubuntu server and install/enable Apache2
-data "aws_ami" "ubuntu" {
+// Data source to get ami
+data "aws_ami" "amazon-linux-2" {
   most_recent = true
-
-  owners = ["591542846629"] # Canonical
+  owners = ["amazon"]
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  ami           = data.aws_ami.amazon-linux-2.id
+  instance_type = "t2.micro"
   availability_zone = "us-west-2a"
-
+  key_name = "main-key"
+  // Attach network interface to instance
+  // often wanna make sure they're in the same availability zone
   network_interface {
     network_interface_id = aws_network_interface.test-interface.id  // Attach network interface to instance
     device_index = 0  // One instance can have many interfaces
@@ -136,13 +152,13 @@ resource "aws_instance" "web" {
 
 
   // Multiline strings can use shell-style "here doc" syntax, with the string starting with a marker like <<EOF and then the string ending with EOF on a line of its own. 
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo apt update -y
-              sudo apt install apache2 -y
-              sudo systemctl start apache2
-              sudo bash -c "echo you're first web server > /var/www/html/index.html"
-              EOF
+   user_data = <<-EOF
+                 #!/bin/bash
+                 sudo yum update -y
+                 sudo yum install httpd -y
+                 sudo systemctl start httpd -y
+                 sudo bash -c 'echo your very first web server > /var/www/html/index.html'
+                 EOF
   tags = {
     Name = "web-server"
   }
